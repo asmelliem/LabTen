17103baa26b525cc5be0ec23323de0d78a2d58e7
Ada <asmelliem@gmail.com>
2020-07-30 19:01:49 -0400

    Added all the code to calculate the area and circumference of a circle

---
+++ LabTen/Circle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabTen
+{
+    public class Circle
+    {
+        private double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+                
+        }
+
+        public double CalculateCircumference()
+        {
+            //C = 2(pi)r
+            var circumference = 2 * Math.PI * Radius;
+            return circumference;
+
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            return FormatNumber(CalculateCircumference());
+        }
+
+        public double CalculateArea()
+        {
+            //Area = (pi)r^2
+            var area = (Math.PI) * Math.Pow(Radius, 2);
+            return area;
+
+        }
+
+        public string CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea());
+        }
+
+        private string FormatNumber(double x)
+        {
+            var formattedNumber = Math.Round((Double)x, 2);
+            return formattedNumber.ToString();
+        }
+    }
+}
--- LabTen/Program.cs
+++ LabTen/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LabTen
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        public static void Main(string[] args)
+        {          
+
+            Console.WriteLine("Welcome to the Circle Tester!");
+            List<Circle> circles = new List<Circle>();
+
+            do
+            {                
+                try
+                {
+                    Console.WriteLine("Enter radius:");
+                    var radius = double.Parse(Console.ReadLine());
+
+                    var circle = new Circle(radius);
+                    circles.Add(circle);
+
+                    Console.WriteLine($"Area: {circle.CalculateFormattedArea()}");
+                    Console.WriteLine($"Circumference: {circle.CalculateFormattedCircumference()}");
+                    Console.WriteLine("Continue? (y/n)");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Looks like there was something wrong with your input. Would you like to try again? (y/n)");
+                }
+
+            } while (Console.ReadLine().ToUpper() == "Y");
+
+            Console.WriteLine($"Goodbye! You've created {circles.Count} Circle Object(s).");
         }
     }
 }
