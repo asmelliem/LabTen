82c8cf3274a43e0da26748ca1cf459f2730cb937
Ada <asmelliem@gmail.com>
2020-07-30 18:08:09 -0400

    Initialize Repo

---
+++ LabTen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabTen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
