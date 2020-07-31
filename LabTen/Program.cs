b8a834f817de08c53168968f4d758ef2932b23b3
Ada <asmelliem@gmail.com>
2020-07-30 20:44:16 -0400

    Added validator class

--- LabTen/Program.cs
+++ LabTen/Program.cs
@@ -12,24 +12,26 @@ namespace LabTen
             List<Circle> circles = new List<Circle>();
 
             do
-            {                
-                try
-                {
-                    Console.WriteLine("Enter radius:");
-                    var radius = double.Parse(Console.ReadLine());
+            {
 
-                    var circle = new Circle(radius);
+                Console.WriteLine("Enter radius:");
+
+                var input = Console.ReadLine();
+                var validator = new Validator(input);
+
+                if(validator.CheckIfDouble())
+                {
+                    var circle = new Circle(validator.ConvertToDouble());
                     circles.Add(circle);
 
                     Console.WriteLine($"Area: {circle.CalculateFormattedArea()}");
                     Console.WriteLine($"Circumference: {circle.CalculateFormattedCircumference()}");
                     Console.WriteLine("Continue? (y/n)");
                 }
-                catch (Exception)
+                else
                 {
                     Console.WriteLine("Looks like there was something wrong with your input. Would you like to try again? (y/n)");
-                }
-
+                }             
             } while (Console.ReadLine().ToUpper() == "Y");
 
             Console.WriteLine($"Goodbye! You've created {circles.Count} Circle Object(s).");
---
+++ LabTen/Validator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabTen
+{
+    class Validator
+    {
+
+        public string Input { get; set; }
+
+        public Validator(string input)
+        {
+            Input = input;               
+        }
+
+        public bool CheckIfDouble()
+        {
+            return Double.TryParse(Input, out var radius);
+        }
+
+        public double ConvertToDouble()
+        {
+            return Convert.ToDouble(Input);
+        }
+    }
+}
